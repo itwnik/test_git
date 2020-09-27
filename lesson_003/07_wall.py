b59acc089bd5367092241513a6d9ae1de1b3e8c5
itwnik@ya.ru <skgl204909>
2020-09-27 19:13:30 +0300

    rep for less_3 works 07 corrected

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -8,15 +8,13 @@ import simple_draw as sd
 
 sd.resolution = (800, 600)
 
-# TODO для того чтобы в коде не распаковывать coordinate_y,
-#  сразу в цикле получим index, coordinate_y in enumerate(range(0, 601, 50)):
-for coordinate_y in enumerate(range(0, sd.resolution[1], 50)):
+for index, coordinate_y in enumerate(range(0, sd.resolution[1], 50)):
     start_point = 0
-    if coordinate_y[0] % 2 == 0:
+    if index % 2 == 0:
         start_point = 50
     for coordinate_x in range(start_point, sd.resolution[0], 100):
-        left_bottom = sd.get_point(coordinate_x, coordinate_y[1])
-        right_top = sd.get_point(coordinate_x + 100, coordinate_y[1] + 50)
+        left_bottom = sd.get_point(coordinate_x, coordinate_y)
+        right_top = sd.get_point(coordinate_x + 100, coordinate_y + 50)
         sd.rectangle(left_bottom,  right_top, color=sd.COLOR_WHITE, width=1)
 
 # Подсказки:
