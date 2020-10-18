0a63883cbbf85df917c153ef731cacc96e925053
itwnik@ya.ru <skgl204909>
2020-10-18 10:59:28 +0300

    rep for less_4 work 05 fix

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -112,62 +112,32 @@ N = 20
 
 sd.resolution = (1200, 600)
 settings_snowflake = []
-fallen_snow = []
 for _ in range(N):
     x = sd.random_number(100, 1200)
     y = sd.random_number(500, 600)
     length = sd.random_number(20, 40)
     settings_snowflake.append([x, y, length])
 
-# Выполнение задачи по Вашему алгоритму, как
-
-# while True:
-#     sd.clear_screen()
-#     # sd.start_drawing()
-#     for index in range(N):
-#         point_x = settings_snowflake[index][0]
-#         point_y = settings_snowflake[index][1]
-#         center_snowflake = sd.get_point(settings_snowflake[index][0], settings_snowflake[index][1])  # рисуем снежинку
-#         settings_snowflake[index][0] += sd.random_number(-15, 15)  # рандомное отклонение вправо/влево
-#         settings_snowflake[index][1] -= sd.random_number(15, 25)
-#         print(settings_snowflake)
-#         sd.snowflake(center=center_snowflake, length=settings_snowflake[index][2])
-#         if point_y < 50:  # если снежинка упала вниз
-#             settings_snowflake[index][1] = 600  # начинаем снегопад заново
-#             fallen_snow.append([point_x, point_y, settings_snowflake[index][2]])  # добовляем координаты ее в список
-#         for index_1 in range(len(fallen_snow)):  # отрисовываем каждую упавшую снежинку из списка
-#             center_fallen_snow = sd.get_point(fallen_snow[index_1][0], fallen_snow[index_1][1])
-#             sd.snowflake(center=center_fallen_snow, length=settings_snowflake[index][2])
-#     # sd.finish_drawing()
-#     sd.sleep(0.1)
-#     if sd.user_want_exit():  # проверка выхода из цикла
-#         break
-
-# второй вариант, выдуманный мной.
-
+end = 50
 
 while True:
     sd.start_drawing()
     for index in range(N):
-        # TODO тут мы объявили point_x point_y, нужно еще и length
-        point_x = settings_snowflake[index][0]
-        point_y = settings_snowflake[index][1]
-        # TODO вот тут в качестве аргументов как раз и передаем point_x point_y, length
-        center_snowflake = sd.get_point(settings_snowflake[index][0], settings_snowflake[index][1])  # координаты
-        # TODO тут мы печатаем sd.snowflake(... color=sd.background_color)
-        # TODO это переносим в конец цикла
-        if point_y < 50:
-            settings_snowflake[index][1] = 600
-        # TODO эту часть else убираем
-        else:
-            sd.snowflake(center=center_snowflake, length=settings_snowflake[index][2], color=sd.background_color)
-        # TODO аналогично изменяем point_x point_y а не сразу в списке!
-        settings_snowflake[index][0] += sd.random_number(-15, 15)
-        settings_snowflake[index][1] -= sd.random_number(15, 25)
-        # TODO в качестве аргументов передаем point_x point_y, length
-        center_snowflake = sd.get_point(settings_snowflake[index][0], settings_snowflake[index][1])  # координаты
-        sd.snowflake(center=center_snowflake, length=settings_snowflake[index][2])
-        # TODO и после того как отрисовали мы сохраняем point_x point_y, length в нужную ячейку в список списков
+        point_x = settings_snowflake[index][0]  # распаковываем х в переменную
+        point_y = settings_snowflake[index][1]  # распаковываем у в переменную
+        length_snow = settings_snowflake[index][2]  # распаковываем длину в переменную
+        center_snowflake = sd.get_point(point_x, point_y)  # получаем центр снежинки
+        sd.snowflake(center_snowflake, length=length_snow, color=sd.background_color)  # рисуем синим снежинку синим
+        point_x += sd.random_number(-15, 15)  # меняем х
+        point_y -= sd.random_number(15, 25)  # меняем у
+        center_snowflake = sd.get_point(point_x, point_y)  # получаем центр новой снежинки
+        sd.snowflake(center=center_snowflake, length=length_snow)  # рисуем новую снежинку белым цветом
+        settings_snowflake[index][0] = point_x  # закпаковываем новые значения х в список
+        settings_snowflake[index][1] = point_y  # запаковываем новые значения у в список
+        settings_snowflake[index][2] = length_snow  # запаковываем новую длину в список
+        if point_y < end:  # если у меньш почти конца экрана
+            settings_snowflake[index][1] = 600  # в списке у определяем в начала экрана
+            end += 1  # увеличиваем почти конец экрана
         sd.finish_drawing()
     sd.sleep(0.1)
     if sd.user_want_exit():  # проверка выхода из цикла
@@ -175,7 +145,7 @@ while True:
 
 sd.pause()
 
-# TODO сугроб доработаем иным способом потому что создания нового списка через 5-10 мин будет тормозить программу!
-# TODO сделаем point_y < 50:  число 50 вынесем в переменную
-#  и сделаем динамическим изменяемым и на каждой итерации будем прибавлять +=0.5 или 1
+# TODO пока не расписал себе весь алгоритм не мог понять почему мы сначала рисуем синюю снежинку, потом белую
+#  Благодарю!!!
+#  Зачем мы распаковываем x, y, length, по идеи если будем работать напрямую из списка, сократим ведь  код?
 
