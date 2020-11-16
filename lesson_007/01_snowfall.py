be66674a82414b3c5e27cf40252670c7547b3c6e
itwnik@ya.ru <skgl204909>
2020-11-16 13:00:24 +0300

    rep for less_7 work 1 bug

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -9,22 +9,41 @@ import simple_draw as sd
 
 
 class Snowflake:
-    pass
 
-    # TODO здесь ваш код
+    def __init__(self):
+        self.x = sd.random_number(50, 550)
+        self.y = sd.random_number(550, 580)
+        self.length = sd.random_number(20, 24)
 
+    def move(self):
+        self.x += sd.random_number(-15, 15)  # меняем х
+        self.y -= sd.random_number(15, 25)  # меняем у
 
-flake = Snowflake()
+    def draw(self, snowflake_color):
+        center_snowflake = sd.get_point(self.x, self.y)  # получаем центр снежинки
+        sd.snowflake(center_snowflake, length=self.length, color=snowflake_color)  # рисуем синим снежинку
 
-while True:
-    flake.clear_previous_picture()
-    flake.move()
-    flake.draw()
-    if not flake.can_fall():
-        break
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
+    def can_fall(self):
+        if self.y < -30:
+            return False
+        else:
+            return True
+
+    def __str__(self):
+        return 'Snowflake: ' + ', '.join(self.x) + ', '.join(self.y) + ', '.join(self.length)
+
+#  ШАГ 1
+# flake = Snowflake()
+#
+# while True:
+#     flake.draw(snowflake_color=sd.background_color)
+#     flake.move()
+#     flake.draw(snowflake_color=sd.COLOR_WHITE)
+#     if not flake.can_fall():
+#         break
+#     sd.sleep(0.1)
+#     if sd.user_want_exit():
+#         break
 
 # шаг 2: создать снегопад - список объектов Снежинка в отдельном списке, обработку примерно так:
 # flakes = get_flakes(count=N)  # создать список снежинок
@@ -39,5 +58,49 @@ while True:
 #     sd.sleep(0.1)
 #     if sd.user_want_exit():
 #         break
+
+#  ШАГ 2
+
+
+def get_flakes(quantity):
+    flakes_roster = []
+    for _ in range(quantity):
+        flakes_roster.append(Snowflake())
+    return flakes_roster
+
+
+def get_fallen_flakes():
+    count = 0
+    for fl in flakes:
+        if not flake.can_fall():
+            count += 1
+            print(flakes.count(fl))
+            del flakes[flakes.count(fl)]
+            # print(str(flakes))
+            # как удалить снежинку?
+    return count
+
+
+def append_flakes(quantity):
+    flakes.extend(get_flakes(quantity))
+
+
+flakes = get_flakes(quantity=3)
+while True:
+    for flake in flakes:
+        sd.start_drawing()
+        flake.draw(snowflake_color=sd.background_color)
+        flake.move()
+        flake.draw(snowflake_color=sd.COLOR_WHITE)
+        sd.finish_drawing()
+    fallen_flakes = get_fallen_flakes()  # подчитать сколько снежинок уже упало
+    # print(fallen_flakes)
+    if fallen_flakes:
+        append_flakes(fallen_flakes)
+        print(len(flakes))
+
+    sd.sleep(0.1)
+    if sd.user_want_exit():
+        break
 
 sd.pause()
