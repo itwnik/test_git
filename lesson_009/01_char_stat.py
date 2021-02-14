7b8c8e8bee9012b49df5c3ee97e7069d0760288e
itwnik@ya.ru <skgl204909>
2021-02-14 18:20:17 +0300

    rep for less_9 work_1 initial test

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -26,31 +26,40 @@
 #   и https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
 
 # TODO здесь ваш код
-# class CollectingStatistics():
-#
-#     def __init__(self, file_name):
+class CollectingStatistics:
 
-statistic = {}
-count = 0
-file_name = 'voyna-i-mir-tom-1.txt'
-with open(file_name, 'r', encoding='cp1251') as file:
-    for line in file:
-        char = ' '
-        for char in line:
-            # print(char)
-            # if char in statistic:
-            #     statistic[char] = statistic.get(char) + 1
-            # else:
-            #     statistic[char] = 1
-            if char.isalpha():
-                if char.isupper():
-                    char = char.lower()
-                if char not in statistic:
-                    statistic[char] = 1
-                else:
-                    statistic[char] = statistic.get(char) + 1
-        # break
+    def __init__(self, file_name):
+        self.file_name = file_name #'voyna-i-mir-tom-1.txt'
+        self.statistic = {}
+        self.char = ' '
 
+    def statistics(self):
+        with open(self.file_name, 'r', encoding='cp1251') as file:
+            for line in file:
+                for self.char in line:
+                    if self.char.isalpha():
+                        if self.char.isupper():
+                            self.char = self.char.lower()
+                        if self.char not in self.statistic:
+                            self.statistic[self.char] = 1
+                        else:
+                            self.statistic[self.char] = self.statistic.get(self.char) + 1
+
+    def output(self):
+        # print('+' * 31)
+        # print('|{txt:^14}|{txt2:^14}|'.format(txt='Буква', txt2='Частота'))
+        # print('+' * 31)
+        for count in range(len(self.statistic)):
+            print(self.statistic)
+        #     print('|{txt:^14}|{txt2:^14}|'.format(txt='Буква', txt2='Частота'))
+        # print('+' * 31)
+        # print(self.statistic)
+        # print(len(self.statistic))
+
+
+test = CollectingStatistics('voyna-i-mir-tom-1.txt')
+test.statistics()
+test.output()
     # print(statistic)
     # print(len(statistic))
 # После зачета первого этапа нужно сделать упорядочивание статистики
