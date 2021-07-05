0e0c5f9bf7a27eb5d48f99434424e6dbb5debd89
itwnik@ya.ru <skgl204909>
2021-07-05 22:23:16 +0300

    rep for less_9 work_1 wow completed

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -32,36 +32,41 @@ class CollectingStatistics:
         self.statistic = {}
         self.char = ' '
 
-    def statistics(self):
+    def file_work(self):
         with open(self.file_name, 'r', encoding='cp1251') as file:
             for line in file:
-                # TODO большая вложенность вынести внутренний метод
                 for self.char in line:
-                    if self.char.isalpha():
-                        if self.char not in self.statistic:
-                            self.statistic[self.char] = 1
-                        else:
-                            self.statistic[self.char] = self.statistic.get(self.char) + 1
+                    self.statistics()
 
-    # TODO напишите метод сортировки отдельно в методе по дефолту Упорядочивание по частоте - по убыванию
+    def statistics(self):
+        if self.char.isalpha():
+            if self.char not in self.statistic:
+                self.statistic[self.char] = 1
+            else:
+                self.statistic[self.char] = self.statistic.get(self.char) + 1
+
+    def sorting_item(self):
+        dictionary_in_list = list(self.statistic.items())
+        # TODO всю голову сломал как назвать эту переменную. Так можно?
+        dictionary_in_list.sort(key=lambda element: element[1])
+        self.statistic = dict(dictionary_in_list)
 
     def output(self):
-        total = 0
+        self.sorting_item()
+        total_quantity_char = 0
         print('+' * 31)
-        print('|{txt:^14}|{txt2:^14}|'.format(txt='Буква', txt2='Частота'))
+        print('|{cell_1:^14}|{cell_2:^14}|'.format(cell_1='Буква', cell_2='Частота'))
         print('+' * 31)
-        # TODO переменные называйте более развернуто, сразу в заголовке цикла получайте
-        # TODO символ и его количество
-        for key in self.statistic:
-            print('|{txt:^14}|{txt2:^14}|'.format(txt=key, txt2=self.statistic[key]))
-            total = self.statistic[key] + total
+        for char_key, frequency in self.statistic.items():
+            print('|{cell_1:^14}|{cell_2:^14}|'.format(cell_1=char_key, cell_2=frequency))
+            total_quantity_char = self.statistic[char_key] + total_quantity_char
         print('+' * 31)
-        print('|{txt:^14}|{txt2:^14}|'.format(txt='Итого', txt2=total))
+        print('|{cell_1:^14}|{cell_2:^14}|'.format(cell_1='Итого', cell_2=total_quantity_char))
         print('+' * 31)
 
 
 analysis = CollectingStatistics('voyna-i-mir-tom-1.txt')
-analysis.statistics()
+analysis.file_work()
 analysis.output()
 
 # После зачета первого этапа нужно сделать упорядочивание статистики
