6cf5cd068e3bf9baf1f65a36b0cf2d0e2e2df4ac
itwnik@ya.ru <skgl204909>
2021-07-06 14:16:02 +0300

    rep for less_9 work_2 without template

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -23,7 +23,33 @@
 #   см https://refactoring.guru/ru/design-patterns/template-method
 #   и https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
 
-# TODO здесь ваш код
+class LogParsing:
+
+    def __init__(self, log_name):
+        self.log_name = log_name
+        self.event_counter = {}
+
+    def parsing(self):
+        with open(self.log_name, 'r') as file:  # 'events2.txt'
+            for line in file:
+                if line[29:32] == 'NOK':
+                    event = line[0:17]
+                    if event not in self.event_counter:
+                        self.event_counter[event] = 1
+                    else:
+                        self.event_counter[event] = self.event_counter.get(event) + 1
+
+    def writing_file(self):
+        with open('parsing_file_out.txt', 'w') as out:
+            for error_time, errors_count in self.event_counter.items():
+                out.write(f"{error_time}] {errors_count} \n")
+        print("Parsing Done!")
+
+
+test = LogParsing('events2.txt')
+test.parsing()
+test.writing_file()
+
 
 # После зачета первого этапа нужно сделать группировку событий
 #  - по часам
