dd8f48457112e8cb4d6811c0822482dd2a20f149
itwnik@ya.ru <skgl204909>
2021-07-12 14:34:21 +0300

    rep for less_9 work_2 sort corrected

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -46,39 +46,19 @@ class LogParsing:
                 self.parsing(line)
 
     def writing_file(self):
-        self.sorting(self.select_sort_from_user())
+        self.sorting()
         with open('parsing_file_out.txt', 'w', encoding='utf8') as out:
             for error_time, errors_count in self.event_counter.items():
                 out.write(f"{error_time}] {errors_count} \n")
             out.write(f" Общее количество ошиибок {self.count}")
         print("Parsing Done!")
 
-    # TODO этот метод нам не нужен
-    def select_sort_from_user(self):
-        while True:
-            user_select = input(f"Выбирите группировку: \n [1] - по часам \n [2] - по месяцу \n [3] - по году \n")
-            if user_select.isdigit() and 1 <= int(user_select) <= 3:
-                return user_select
-            print(f"Ошибка! повторите ввод!")
-
-    # TODO в этот методе должна быть только одна по дефолту сортировка
-    def sorting(self, user_select):
-        if user_select == '1':  # по часу
-            start_sorting_position, end_sorting_position = 12, 14
-        elif user_select == '2':  # по месяцу
-            start_sorting_position, end_sorting_position = 6, 8
-        elif user_select == '3':  # по году
-            start_sorting_position, end_sorting_position = 1, 5
+    def sorting(self):
         output_data_sorting = list(self.event_counter.items())
-        output_data_sorting.sort(key=lambda element: element[0][start_sorting_position:end_sorting_position])
+        output_data_sorting.sort(key=lambda element: element[0][1:5])  # сортировка по году
         self.event_counter = dict(output_data_sorting)
 
-    # т.е. у нас будет 1 класс? и внем отдельно меттод сортировки? так?
-    # тогда в чем фишка шаблонного метода?
-    # TODO чтобы в дочерних классах переопределить только этот метод и не более
-    # TODO https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
 
-# TODO Сделайте сначала первый этап
 parsing_file = LogParsing('events.txt')
 parsing_file.reading_file()
 parsing_file.writing_file()
