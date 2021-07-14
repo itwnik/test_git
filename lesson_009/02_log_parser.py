a0340a844fe133d6d4c46581b580bd1c49e2d9e3
user74 <housechel@mail.ru>
2021-07-14 13:03:08 +0500

    Lesson 009 02 - Corrected

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -54,6 +54,7 @@ class LogParsing:
         print("Parsing Done!")
 
     def sorting(self):
+        # TODO попробуйте сократить до одной строки, как бы много преобразований
         output_data_sorting = list(self.event_counter.items())
         output_data_sorting.sort(key=lambda element: element[0][1:5])  # сортировка по году
         self.event_counter = dict(output_data_sorting)
