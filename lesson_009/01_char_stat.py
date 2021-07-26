2d596b8c5bf3f5e26e4d3b4021014d3e23707fcf
itwnik@ya.ru <skgl204909>
2021-07-26 14:35:14 +0300

    rep for less_9 work_1 wow corrected

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -25,10 +25,13 @@
 #   см https://refactoring.guru/ru/design-patterns/template-method
 #   и https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
 
+BOOK = 'voyna-i-mir.txt'
+
+
 class CollectingStatistics:
 
     def __init__(self, file_name):
-        self.file_name = file_name  # 'voyna-i-mir-tom-1.txt'
+        self.file_name = file_name
         self.statistic = {}
         self.char = ' '
 
@@ -45,14 +48,12 @@ class CollectingStatistics:
             else:
                 self.statistic[self.char] = self.statistic.get(self.char) + 1
 
-
-    def sorting_item(self):  # TODO Определения методов отделяются от остального кода одной пустой строкой
+    def sorting_item(self):
         output_data = list(self.statistic.items())
         output_data.sort(key=lambda element: element[1])
         self.statistic = dict(output_data)
 
     def output(self):
-        self.sorting_item()
         total_quantity_char = 0
         print('+' * 31)
         print('|{cell_1:^14}|{cell_2:^14}|'.format(cell_1='Буква', cell_2='Частота'))
@@ -63,31 +64,18 @@ class CollectingStatistics:
         print('+' * 31)
         print('|{cell_1:^14}|{cell_2:^14}|'.format(cell_1='Итого', cell_2=total_quantity_char))
         print('+' * 31)
-    # TODO Для реализации паттерна необходимо разбить всю выполняемую задачу на отдельные шаги:
-    #  1) получение данных (тут может быть подготовка файла - его разахивация, при необходимости),
-    #  2) сбор даннных - подсчёт частоты использования букв,
-    #  3) сортировка статистических данных,
-    #  4) вывод в консоль таблицы с данными.
-    #  Также потребуется сам "шаблонный метод", где последовательно вызываются все методы по порядку.
-    #    а) подготовка файла (разархивация, при необходимости)
-    #    б) сбор данных из файла
-    #    в) сортировка
-    #    г) вывод в консоль
-    #  Имя шаблонного метода должно быть кратким, например: "запустить", "выполнить"
+
+    def starting(self):
+        self.file_work()
+        self.statistics()
+        self.sorting_item()
+        self.output()
 
 
-#  теперь да))  до этого свой файл качал, не заметил его в сниппетах
-analysis = CollectingStatistics('voyna-i-mir.txt')
-# TODO Имена файлов надо присваивать константам и использовать в основном коде только их.
-#  Имена констант пишутся большими буквами. Располагают константы в начале модуля, сразу после
-#  импортов сторонних модулей.
-#  Может возникнуть необходимость изменить имя файла и через константу это делать удобнее - константа это
-#  единое место изменения, а примениться она может во многих местах. Поэтому вверху её легко найти для изменения
-#  без необходимости перелопачивания кода проекта.
+analysis = CollectingStatistics(BOOK)
+analysis.starting()
 
-analysis.file_work()
-analysis.output()
-# TODO тут должен быть вызван только один метод - шаблонный
+# TODO Благодарю за разъяснения. Поправил в соответствии с рекоммендациями.
 
 # После зачета первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
