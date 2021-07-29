cb3485306aacaa5cb655cf3a6e3605bfe1945db6
itwnik@ya.ru <skgl204909>
2021-07-29 19:55:10 +0300

    rep for less_9 work_1 part_2 wow completed

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -34,6 +34,7 @@ class CollectingStatistics:
         self.file_name = file_name
         self.statistic = {}
         self.char = ' '
+        self.total_quantity_char = 0
 
     def file_work(self):
         with open(self.file_name, 'r', encoding='cp1251') as file:
@@ -49,48 +50,41 @@ class CollectingStatistics:
                 self.statistic[self.char] = self.statistic.get(self.char) + 1
 
     def sorting_item(self):
-        self.statistic = dict(sorted(self.statistic.items(), key=lambda element: element[1]))
+        for char_key, frequency in sorted(self.statistic.items(), key=lambda element: element[1]):
+            print('|{cell_1:^14}|{cell_2:^14}|'.format(cell_1=char_key, cell_2=frequency))
+            self.total_quantity_char = self.statistic[char_key] + self.total_quantity_char
 
     def output(self):
-        total_quantity_char = 0
         print('+' * 31)
         print('|{cell_1:^14}|{cell_2:^14}|'.format(cell_1='Буква', cell_2='Частота'))
         print('+' * 31)
-        # TODO вот эту часть кода (только цикл) можно вынести сразу в sorting_item
-        # TODO тут только вызвать метод self.sorting_item()
-        # TODO в самом цикле self.statistic.items() заменить на
-        # TODO sorted(self.statistic.items(), key=lambda element: element[1]) который вернут тьюпол
-        for char_key, frequency in self.statistic.items():
-            print('|{cell_1:^14}|{cell_2:^14}|'.format(cell_1=char_key, cell_2=frequency))
-            total_quantity_char = self.statistic[char_key] + total_quantity_char
+        self.sorting_item()
         print('+' * 31)
-        print('|{cell_1:^14}|{cell_2:^14}|'.format(cell_1='Итого', cell_2=total_quantity_char))
+        print('|{cell_1:^14}|{cell_2:^14}|'.format(cell_1='Итого', cell_2=self.total_quantity_char))
         print('+' * 31)
 
     def starting(self):
         self.file_work()
-        self.sorting_item()
         self.output()
 
+# TODO Почему три дочерних класса? сортировка по частоте у нас в родительском классе.
+#  и 2 дочерних "по алфавиту возрастанию" и "по по алфавиту по убыванию". Так ведь?
 
-# TODO у нас должно получится что с похожей структурой
-#   и https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
-
-# TODO три дочерних класса в который переопределен только один метод сортировки
-# TODO если нужно поправить родительский класс!
 
 class CollectingStatisticsTwo(CollectingStatistics):
 
-    def __init__(self, file_name, flag):
-        super().__init__(file_name)
-        # self.file_name = file_name
-        self.flag = flag
+    def sorting_item(self):
+        for char_key, frequency in sorted(self.statistic.items(), key=lambda element: element[0], reverse=True):  # воз
+            print('|{cell_1:^14}|{cell_2:^14}|'.format(cell_1=char_key, cell_2=frequency))
+            self.total_quantity_char = self.statistic[char_key] + self.total_quantity_char
+
+
+class CollectingStatisticsThree(CollectingStatistics):
 
     def sorting_item(self):
-        if self.flag == '2':
-            self.statistic = dict(sorted(self.statistic.items(), key=lambda element: element[0], reverse=True))  # воз
-        else:
-            self.statistic = dict(sorted(self.statistic.items(), key=lambda element: element[0], reverse=False))  # убыв
+        for char_key, frequency in sorted(self.statistic.items(), key=lambda element: element[0], reverse=False):  # уб
+            print('|{cell_1:^14}|{cell_2:^14}|'.format(cell_1=char_key, cell_2=frequency))
+            self.total_quantity_char = self.statistic[char_key] + self.total_quantity_char
 
 
 def select_sort_from_user():
@@ -110,13 +104,10 @@ if __name__ == '__main__':
     if user_select == '1':  # по частоте по возрастанию
         analysis = CollectingStatistics(BOOK)
     elif user_select == '2':  # по алфавиту по возрастанию
-        analysis = CollectingStatisticsTwo(BOOK, user_select)
+        analysis = CollectingStatisticsTwo(BOOK)
     else:  # по алфавиту по убыванию
-        analysis = CollectingStatisticsTwo(BOOK, user_select)
+        analysis = CollectingStatisticsThree(BOOK)
     analysis.starting()
-
-# метод сортировки долго работает. Как его можно ускорить?
-# TODO сильно много преобразований
 
 # После зачета первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
