2acbd3947529ce15ec5e8e89ac77fa0034d7ced1
itwnik@ya.ru <skgl204909>
2021-08-09 12:34:18 +0300

    rep for less_10 work_3 completed

--- lesson_010/03_registration_log.py
+++ lesson_010/03_registration_log.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+"""
 # Есть файл с протоколом регистраций пользователей на сайте - registrations.txt
 # Каждая строка содержит: ИМЯ ЕМЕЙЛ ВОЗРАСТ, разделенные пробелами
 # Например:
@@ -21,6 +22,7 @@
 # - поле емейл НЕ содержит @ и .(точку): NotEmailError (кастомное исключение)
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
+"""
 
 FILE = 'registrations.txt'
 
@@ -33,41 +35,37 @@ class NotEmailError(ValueError):
     pass
 
 
-# TODO переменная line не должна быть названа одинакова с глобальной
-def file_check(line):
-    global msg
-    pars_text = line.split(' ')
+def file_check(check_line):
+    global message
+    pars_text = check_line.split(' ')
     if len(pars_text) != 3:
-        msg = " ".join(pars_text)
+        message = " ".join(pars_text)
         raise ValueError('НЕ присутсвуют все три поля')
     elif not pars_text[0].isalpha():
-        msg = " ".join(pars_text)
+        message = " ".join(pars_text)
         raise NotNameError('поле имени содержит НЕ только буквы')
     elif pars_text[1].count('@') != 1 and pars_text[1].count('.') != 1:
-        msg = " ".join(pars_text)
+        message = " ".join(pars_text)
         raise NotEmailError('поле емейл НЕ содержит @ и .(точку)')
     elif int(pars_text[2]) <= 10 or int(pars_text[2]) >= 99:
-        msg = " ".join(pars_text)
+        message = " ".join(pars_text)
         raise ValueError('поле возраст НЕ является числом от 10 до 99')
     else:
-        msg = " ".join(pars_text)
+        message = " ".join(pars_text)
 
 
-# TODO написать одну общею функцию записи
-def output_good(out_line):
-    with open('registrations_good.log', 'w', encoding='utf8') as out:
-        for item in out_line:
-            out.write(f"{item}")
+# TODO наверно лучше открыть каждый файл для записи, а не 2 сразу?
+#   если все же получается длинная строка, как ее переносить на другую? так же как предлогает PyCharm через "\"?
+def output_files(out_good, out_bad):
+    with open('registrations_good.log', 'w', encoding='utf8') as out_good_log,\
+         open('registrations_bad.log', 'w', encoding='utf8') as out_bad_log:
+        for item in out_good:
+            out_good_log.write(f"{item}")
+        for item in out_bad:
+            out_bad_log.write(f"{item}")
 
 
-def output_bad(out_line):
-    with open('registrations_bad.log', 'w', encoding='utf8') as out:
-        for item in out_line:
-            out.write(f"{item}")
-
-
-# TODO сокращения переменных в коде быть не должно
-msg = ''
+message = ''
 good = []
 bad = []
 
@@ -75,10 +73,9 @@ with open(FILE, 'r', encoding='utf-8') as file:
     for line in file:
         try:
             file_check(line)
-            good.append(msg)
+            good.append(message)
         except (ValueError, NotNameError, NotEmailError) as exc:
-            out_test = " {} {} {}\n".format(msg.rstrip('\n'), str(type(exc)),  str(exc.args[0]))
+            out_test = " {} {} {}\n".format(message.rstrip('\n'), str(type(exc)),  str(exc.args[0]))
             bad.append(out_test)
-output_good(good)
-output_bad(bad)
+output_files(good, bad)
 print("Parsing done!")
