60584c87466c42c621695ac93e170c3e36128410
itwnik@ya.ru <skgl204909>
2021-08-09 12:34:35 +0300

    rep for less_10 work_2 completed

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -16,7 +16,9 @@
 # кармы до уровня ENLIGHTENMENT_CARMA_LEVEL. Исключения обработать и записать в лог.
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
+
 import random as rd
+import os
 
 ENLIGHTENMENT_CARMA_LEVEL = 777
 
@@ -45,46 +47,55 @@ class SuicideError(FileExistsError):
     pass
 
 
+ERROR = [IamGodError, DrunkError, CarCrashError, GluttonyError, DepressionError, SuicideError]
+karma = 0
 count = 0
-karma_quantity = 0
-
-# TODO создайте константу список в котором будут храниться имена class ов чтобы их потом можно было выбрать и вызвать!
-# TODO имена классов храним без их вызовов () и сообщений, сами сообщения будем формировать в главном цикле
-error = [IamGodError('Эта Ошибка Божествнна!'),
-         DrunkError('Ошибка: Балтика 9! И пусть весь мир подождет'),
-         CarCrashError('Ошибка: была когда ты сел за руль этой Калины!'),
-         GluttonyError('Ошибка: Переел в Макдональдсе'),
-         DepressionError('Ошибка: Depression detect!'),
-         SuicideError('Они убили Кенни')]
+error_logs = []
 
 
-# TODO функция one_day() должна возвращать карму от 1 до 7 или рейзит ошибку из расчета 1 к 13
-# TODO мы можем объявить 2 переменные это карма равная рендинт от 1 до 7 и
-# TODO сам еррор который тоже равен рендинт от 1 до 13
-# TODO далее условие если еррор равен 13 то мы choice выбираем случайное исключение из списка
-# TODO и его рейзим как объект используя ()
-# TODO если условие не сработало то мы ретурним карму
 def one_day():
-    # TODO без глобал
-    global karma_quantity
-    karma_quantity += rd.randint(1, 7)
-    probability = rd.randint(1, 13)
-    if probability == 13:
-        raise rd.choice(error)
+    karma_quantity = rd.randint(1, 7)
+    error = rd.randint(1, 13)
+    if error == 13:
+        raise rd.choice(ERROR)()
+    else:
+        return karma_quantity
 
 
- while karma_quantity <= ENLIGHTENMENT_CARMA_LEVEL:
+def error_log(error_code):
+    message = ''
+    if error_code.__class__.__name__ == 'IamGodError':
+        message = 'Эта Ошибка Божествнна!'
+    elif error_code.__class__.__name__ == 'DrunkError':
+        message = 'Ошибка: Балтика 9! И пусть весь мир подождет'
+    elif error_code.__class__.__name__ == 'CarCrashError':
+        message = 'Ошибка: была когда ты сел за руль этой Калины!'
+    elif error_code.__class__.__name__ == 'GluttonyError':
+        message = 'Ошибка: Переел в Макдональдсе'
+    elif error_code.__class__.__name__ == 'DepressionError':
+        message = 'Ошибка: Depression detect!'
+    elif error_code.__class__.__name__ == 'SuicideError':
+        message = 'Они убили Кенни!'
+    error_logs.append(f"{count}. Error Detected {exc.__class__.__name__}: {message}, Карма = {karma}")
+    with open('error.log', 'w') as file:
+        for item in error_logs:
+            file.write(f"{item}\n")
+
+
+while karma <= ENLIGHTENMENT_CARMA_LEVEL:
     try:
-        one_day()
-    # TODO написать отдельную функцию записи, тут будем наполнять список, формируя нужную стоку
-    # TODO в конце цикла нужно запустить функцию записи
-    # TODO строку формируем на каждой ошибке свою
+        karma += one_day()
     except (IamGodError, DrunkError, CarCrashError, GluttonyError, DepressionError, SuicideError) as exc:
         count += 1
-        print(f"{count}. Error Detected {type(exc)}: {exc}, Карма = {karma_quantity}")
-    # TODO можно достучаться до имени класса через меджик метод __init__
+        error_log(exc)
+
+    # TODO как через __init__? В сети нашел так __class__.__name__
+    # можно достучаться до имени класса через меджик метод __init__
     # как убрать "<class '__main__." в выводе типа своей ошибки?
+
 print(f"Количество зарегистрированных ошибок: {count}")
-print(f"Карма достикла уровня ПРОСВЕТЛЕНИЯ и = {karma_quantity}, день сурка закончен!")
+print(f"Карма достикла уровня ПРОСВЕТЛЕНИЯ и = {karma}, день сурка закончен!")
+print("log create!")
+os.startfile('error.log')
 
 # https://goo.gl/JnsDqu
