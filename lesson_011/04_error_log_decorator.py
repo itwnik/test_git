bfb5d8af456536f547a824632cba7cb317777d47
itwnik@ya.ru <skgl204909>
2021-08-21 20:24:39 +0300

    rep for less_11 work_4 completed

--- lesson_011/04_error_log_decorator.py
+++ lesson_011/04_error_log_decorator.py
@@ -8,25 +8,81 @@
 # Лог файл открывать каждый раз при ошибке в режиме 'a'
 
 
-def log_errors(func):
-    def log_write(*args, **kwargs):
-        try:
-            result = func(*args, **kwargs)
-            return result
-        except Exception as exc2:
-            with open('function_errors.log', 'a', encoding='utf-8') as file:
-                file.write(f'Error! <имя функции>: {func.__name__}, <параметры вызова>: {args, kwargs} '
-                           f'<тип ошибки>: {exc2.__class__.__name__} <текст ошибки>: {exc2}\n')
-            raise exc2
-    return log_write
+# def log_errors(func):
+#     def log_write(*args, **kwargs):
+#         try:
+#             result = func(*args, **kwargs)
+#             return result
+#         except Exception as exc2:
+#             with open('function_errors.log', 'a', encoding='utf-8') as file:
+#                 file.write(f'Error! <имя функции>: {func.__name__}, <параметры вызова>: {args, kwargs} '
+#                            f'<тип ошибки>: {exc2.__class__.__name__} <текст ошибки>: {exc2}\n')
+#             raise exc2
+#     return log_write
+#
+#
+# @log_errors
+# def perky(param):
+#     return param / 0
+#
+#
+# @log_errors
+# def check_line(line):
+#     name, email, age = line.split(' ')
+#     if not name.isalpha():
+#         raise ValueError("it's not a name")
+#     if '@' not in email or '.' not in email:
+#         raise ValueError("it's not a email")
+#     if not 10 <= int(age) <= 99:
+#         raise ValueError('Age not in 10..99 range')
+#
+#
+# lines = [
+#     'Ярослав bxh@ya.ru 600',
+#     'Земфира tslzp@mail.ru 52',
+#     'Тролль nsocnzas.mail.ru 82',
+#     'Джигурда wqxq@gmail.com 29',
+#     'Земфира 86',
+#     'Равшан wmsuuzsxi@mail.ru 35',
+# ]
+#
+# for line in lines:
+#     try:
+#         check_line(line)
+#     except Exception as exc:
+#         print(f'Invalid format: {exc}')
+# perky(param=42)
+
+# TODO так?
+# Усложненное задание (делать по желанию).
+# Написать декоратор с параметром - именем файла
+
+# @log_errors('function_errors.log')
+# def func():
+#     pass
 
 
-@log_errors
+def log_errors(log_file):
+    def wrapped(func):
+        def log_write(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                return result
+            except Exception as exc2:
+                with open(log_file, 'a', encoding='utf-8') as file:
+                    file.write(f'Error! <имя функции>: {func.__name__}, <параметры вызова>: {args, kwargs} '
+                               f'<тип ошибки>: {exc2.__class__.__name__} <текст ошибки>: {exc2}\n')
+                raise exc2
+        return log_write
+    return wrapped
+
+
+@log_errors('function_errors.log')
 def perky(param):
     return param / 0
 
 
-@log_errors
+@log_errors('function_errors.log')
 def check_line(line):
     name, email, age = line.split(' ')
     if not name.isalpha():
@@ -52,11 +108,3 @@ for line in lines:
     except Exception as exc:
         print(f'Invalid format: {exc}')
 perky(param=42)
-
-# TODO предлагаю вам сделать вторую часть
-# Усложненное задание (делать по желанию).
-# Написать декоратор с параметром - именем файла
-#
-# @log_errors('function_errors.log')
-# def func():
-#     pass
