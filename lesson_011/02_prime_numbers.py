b1a679ee1aa90a5fe392b63d9e1ca9ddf96af97c
itwnik@ya.ru <skgl204909>
2021-08-23 21:23:48 +0300

    rep for less_11 work_2 (3.1, 3.2, 3.3, 3.3.1, 3.3.2) completed

--- lesson_011/02_prime_numbers.py
+++ lesson_011/02_prime_numbers.py
@@ -46,7 +46,6 @@ def get_prime_numbers(n):
 # for number in prime_number_iterator:
 #     print(number)
 
-
 # Часть 2
 # Теперь нужно создать генератор, который выдает последовательность простых чисел до n
 # Распечатать все простые числа до 10000 в столбик
@@ -54,7 +53,6 @@ def get_prime_numbers(n):
 
 def prime_numbers_generator(n):
     prime_numbers = []
-    # TODO тут number подчеркивает из за того что в глобальном скоупе есть такой же нейминг переменной
     for number in range(2, n + 1):
         for prime in prime_numbers:
             if number % prime == 0:
@@ -64,11 +62,8 @@ def prime_numbers_generator(n):
             yield number
 
 
-for number in prime_numbers_generator(n=10000):
-    print(number)
-
-# TODO генератор проще чем итератор
-# TODO Делайте далее
+# for number in prime_numbers_generator(n=10000):
+#     print(number)
 
 
 # Часть 3
@@ -78,11 +73,88 @@ for number in prime_numbers_generator(n=10000):
 #       то для вычисления "счастливости" брать равное количество цифр с начала и конца:
 #           727 -> 7(2)7 -> 7 == 7 -> True
 #           92083 -> 92(0)83 -> 9+2 == 8+3 -> True
+
+# ##Part_3.1##
+def filter_lucky_number(in_number):
+    numbers = list(map(int, str(in_number)))
+    formula = int((len(numbers) - 1) / 2)
+    if len(numbers) >= 3:
+        if sum(numbers[:formula]) == sum(numbers[-formula:]):
+            return True
+        else:
+            return False
+    return False
+
+# TODO для проверки раскомментить
+# for number in prime_numbers_generator(n=10000):
+#     print(number, filter_lucky_number(number))
+# ##################
+
 # 2) "палиндромное" - одинаково читающееся в обоих направлениях. Например 723327 и 101
+
+# ##Part_3.2##
+def filter_lucky_number_two(in_number):
+    numbers = list(map(int, str(in_number)))
+    if len(numbers) >= 2 and numbers == list(reversed(numbers)):
+        return True
+    else:
+        return False
+
+# TODO для проверки раскомментить
+# for number in prime_numbers_generator(n=10000):
+#     print(number, filter_lucky_number_two(number))
+# ##################
+
 # 3) придумать свою (https://clck.ru/GB5Fc в помощь)
-#
+
+# ##Part_3.3##
+def filter_lucky_number_three(in_number):  # Триморфное число https://clck.ru/SoJih
+    if str(in_number) == str(in_number**3)[-len(str(in_number)):]:
+        return True
+    else:
+        return False
+
+# TODO для проверки раскомментить
+# for number in prime_numbers_generator(n=10000):
+#     print(number, filter_lucky_number_three(number))
+# ##################
+
 # Подумать, как можно применить функции-фильтры к полученной последовательности простых чисел
 # для получения, к примеру: простых счастливых чисел, простых палиндромных чисел,
 # простых счастливых палиндромных чисел и так далее. Придумать не менее 2х способов.
 #
 # Подсказка: возможно, нужно будет добавить параметр в итератор/генератор.
+
+# ##Part_3.3.1## method_1
+def prime_numbers_generator_two(n):
+    prime_numbers = []
+    for number in range(2, n + 1):
+        for prime in prime_numbers:
+            if number % prime == 0:
+                break
+        else:
+            prime_numbers.append(number)
+            if filter_lucky_number_three(number):
+                yield number
+
+# TODO для проверки раскомментить
+# for number_2 in prime_numbers_generator_two(n=10000):
+#     print(number_2, filter_lucky_number_three(number_2))
+# ##################
+
+# ##Part_3.3.2## method_2
+def prime_numbers_generator_three(n):
+    prime_numbers = []
+    for number in range(2, n + 1):
+        for prime in prime_numbers:
+            if number % prime == 0:
+                break
+        else:
+            prime_numbers.append(number)
+            yield number
+
+# TODO для проверки раскомментить
+# for number_2 in prime_numbers_generator_three(n=10000):
+#     if filter_lucky_number(number_2):
+#         print(number_2)
+# ##################
