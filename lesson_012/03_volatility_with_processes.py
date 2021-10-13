0135efd3646ff3b020daff8bd68e373de65201a9
itwnik@ya.ru <skgl204909>
2021-10-13 22:55:16 +0300

    rep for less_12 work_3 completed

--- lesson_012/03_volatility_with_processes.py
+++ lesson_012/03_volatility_with_processes.py
@@ -34,15 +34,12 @@ class TickerInspector(multiprocessing.Process):
         self.full_path = full_path
         self.name_ticker = ''
         self.volatility = 0
-        self.informator = informator   # определили очередь
+        self.informator = informator
 
     def run(self):
         prices = self.file_work()
         self.volatility_calculation(prices)
-        # запихнули в очередь данные которые нам отдает класс
-        # на вход лучше передать словарь из двух ключей на значение им передать self.name_ticker,
-        #  self.volatility_calculation
-        self.informator.put(dict(name=self.name_ticker, volatility=self.volatility_calculation))
+        self.informator.put(dict(name=self.name_ticker, volatility=self.volatility))
 
     def file_work(self):
         tickers_prices = []
@@ -62,42 +59,32 @@ class TickerInspector(multiprocessing.Process):
 def main():
     tickers_volatilitys = {}
     tickers_volatilitys_zero = []
-    informator = multiprocessing.Queue()  # создали очередь
-    calculator_volatilitys = [TickerInspector(file_name, informator) for file_name in ut.file_sniffer(PATH)]  # пердали ее в класс
+    informator = multiprocessing.Queue()
+    calculator_volatilitys = [TickerInspector(file_name, informator) for file_name in ut.file_sniffer(PATH)]
 
     for calculator_volatility in calculator_volatilitys:
         calculator_volatility.start()
 
     while True:
         try:
-            # TODO тут добавить timeout = 1 или меньше
-            data = informator.get()
+            data = informator.get(timeout=1)
             name_ticker, volatility = data['name'], data['volatility']
             if volatility <= 0:
                 tickers_volatilitys_zero.append(name_ticker)
             else:
                 tickers_volatilitys[name_ticker] = volatility
         except Empty:
-            # TODO нужно добавить not перед any мы же выходим кода все пусто
-            if any(calculator_volatility.is_alive() for calculator_volatility in calculator_volatilitys):
+            if not any(calculator_volatility.is_alive() for calculator_volatility in calculator_volatilitys):
                 break
 
     for calculator_volatility in calculator_volatilitys:
         calculator_volatility.join()
 
-    # TODO если кооментировать эти две функции проблема исчезает
-    # tickers_volatilitys_max, tickers_volatilitys_min = ut.filter_data(tickers_volatilitys)
-    # ut.print_result(tickers_volatilitys_max, tickers_volatilitys_min, tickers_volatilitys_zero)
+    tickers_volatilitys_max, tickers_volatilitys_min = ut.filter_data(tickers_volatilitys)
+    ut.print_result(tickers_volatilitys_max, tickers_volatilitys_min, tickers_volatilitys_zero)
 
 
 if __name__ == '__main__':
     main()
 
-# Traceback (most recent call last):
-#   File "C:\Python\Python38-32\lib\multiprocessing\queues.py", line 239, in _feed
-#     obj = _ForkingPickler.dumps(obj)
-#   File "C:\Python\Python38-32\lib\multiprocessing\reduction.py", line 51, in dumps
-#     cls(buf, protocol).dump(obj)
-#   File "C:\Python\Python38-32\lib\multiprocessing\process.py", line 347, in __reduce__
-#     raise TypeError(
-# TypeError: Pickling an AuthenticationString object is disallowed for security reasons
+# Функция работала 3.9116 секунд(ы)
