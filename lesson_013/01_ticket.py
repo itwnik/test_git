3cb86af2691c74c7bb88c66cb38c2376ca5f9fd5
itwnik <itwnik@ya.ru>
2022-01-05 15:56:00 +0300

    rep for less_13 work_1_p1 completed

--- lesson_013/01_ticket.py
+++ lesson_013/01_ticket.py
@@ -8,9 +8,33 @@
 # Пример заполнения lesson_013/images/ticket_sample.png
 # Подходящий шрифт искать на сайте ofont.ru
 
+
+from PIL import Image, ImageDraw, ImageFont, ImageColor
+
+TICKET_PATH = "images/ticket_template.png"
+
+
 def make_ticket(fio, from_, to, date):
-    # TODO здесь ваш код
-    pass
+    ticket = Image.open(TICKET_PATH)
+    full_ticket_draw = ImageDraw.Draw(ticket)
+
+    text_fio_ticket = fio
+    text_from_ticket = from_
+    text_to_ticket = to
+    text_date_ticket = date
+    font_ticket = ImageFont.truetype("python_snippets/fonts/ofont.ru_Source_Seri_fPro.ttf", size=14)
+
+    full_ticket_draw.text((45, 122), text_fio_ticket, font=font_ticket, fill=ImageColor.colormap["black"])
+    full_ticket_draw.text((45, 190), text_from_ticket, font=font_ticket, fill=ImageColor.colormap["black"])
+    full_ticket_draw.text((45, 256), text_to_ticket, font=font_ticket, fill=ImageColor.colormap["black"])
+    full_ticket_draw.text((285, 256), text_date_ticket, font=font_ticket, fill=ImageColor.colormap["black"])
+
+    ticket.save("images/full_ticket.png")
+
+
+if __name__ == '__main__':
+    make_ticket('Petrov A.A.', 'Rostov-on-Don', 'Moscow', '01.01.22')
+
 
 # Усложненное задание (делать по желанию).
 # Написать консольный скрипт c помощью встроенного python-модуля argparse.
